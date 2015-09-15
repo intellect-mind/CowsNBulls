349fb1ecbbec497a9a97eb9e8c912b66427d560c
mlnsharma <mlnsharma07@gmail.com>
2015-09-15 19:43:44 +0200

    Fixed GamePage merge conflict

--- app/src/main/java/com/intellectminds/cowsnbulls/activity/GamePageActivity.java
+++ app/src/main/java/com/intellectminds/cowsnbulls/activity/GamePageActivity.java
@@ -1,37 +1,27 @@
 package com.intellectminds.cowsnbulls.activity;
 
 import android.app.Activity;
-<<<<<<< HEAD
 import android.content.Intent;
-=======
 import android.graphics.Color;
->>>>>>> origin/master
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.intellectminds.cowsnbulls.R;
 import com.intellectminds.cowsnbulls.logic.GameLogic;
 
 public class GamePageActivity extends Activity {
 
-    private static boolean createRow = false;
     String msg = "Android : ";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
